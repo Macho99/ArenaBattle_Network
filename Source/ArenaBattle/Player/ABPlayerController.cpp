ec7484040282690b46ad4c1000e8bbb7e8afdf04
Macho99 <pes4825@naver.com>
2026-05-21 03:31:25 +0900

    feat : 1 ~ 3강 내용

---
+++ Source/ArenaBattle/Player/ABPlayerController.cpp
@@ -0,0 +1,61 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/ABPlayerController.h"
+#include "UI/ABHUDWidget.h"
+#include "Kismet/GameplayStatics.h"
+#include "ABSaveGame.h"
+#include "ArenaBattle.h"
+
+DEFINE_LOG_CATEGORY(LogABPlayerController);
+
+AABPlayerController::AABPlayerController()
+{
+	static ConstructorHelpers::FClassFinder<UABHUDWidget> ABHUDWidgetRef(TEXT("/Game/ArenaBattle/UI/WBP_ABHUD.WBP_ABHUD_C"));
+	if (ABHUDWidgetRef.Class)
+	{
+		ABHUDWidgetClass = ABHUDWidgetRef.Class;
+	}
+}
+
+void AABPlayerController::PostInitializeComponents()
+{
+	AB_LOG(LogABNetwork, Log, TEXT("%s"), TEXT("Begin"));
+    Super::PostInitializeComponents();
+	AB_LOG(LogABNetwork, Log, TEXT("%s"), TEXT("End"));
+}
+
+void AABPlayerController::PostNetInit()
+{
+	AB_LOG(LogABNetwork, Log, TEXT("%s"), TEXT("Begin"));
+    Super::PostNetInit();
+
+
+	UNetDriver* NetDriver = GetWorld()->GetNetDriver();
+	if (NetDriver)
+	{
+		AB_LOG(LogABNetwork, Log, TEXT("%s"), *FString::Printf(TEXT("ServerConnection: %s"), *NetDriver->ServerConnection->GetName()));
+	}
+	else
+	{
+		AB_LOG(LogABNetwork, Log, TEXT("%s"), TEXT("No NetDriver"));
+	}
+	AB_LOG(LogABNetwork, Log, TEXT("%s"), TEXT("End"));
+}
+
+void AABPlayerController::BeginPlay()
+{
+	AB_LOG(LogABNetwork, Log, TEXT("%s"), TEXT("Begin"));
+	Super::BeginPlay();
+	AB_LOG(LogABNetwork, Log, TEXT("%s"), TEXT("End"));
+
+	FInputModeGameOnly GameOnlyInputMode;
+	SetInputMode(GameOnlyInputMode);
+}
+
+void AABPlayerController::OnPossess(APawn* InPawn)
+{
+	AB_LOG(LogABNetwork, Log, TEXT("%s"), TEXT("Begin"));
+	Super::OnPossess(InPawn);
+	AB_LOG(LogABNetwork, Log, TEXT("%s"), TEXT("End"));
+}
